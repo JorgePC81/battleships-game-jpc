e2c20a4b2a618752d16e0c126da8e3072bc289a5
Jorge Pallares Catalan <jorge.pallares.catalan@gmail.com>
2023-09-02 23:19:31 +0200

    using the correct template

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_shot():
+    """enabling the player to enter the shot"""
+    
+    ok = "n"
+    while ok == "n":
+        shot = input("please enter your guess: ")
+        shot = int(shot)
+        if shot < 0 or shot > 99:
+            print("incorrect number, try again")
+        else:
+            ok = 'y'
+            break
+    return shot
+    
+
+def battle_board(hit, miss, comp):
+
+    print("          The Battleships Game\n")   # Title
+
+    # making a numeric diagram
+    print ("     0  1  2  3  4  5  6  7  8  9")
+     
+    place = 0  
+    for x in range(10): 
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in miss:
+                ch = " x "
+            elif place in hit:
+                ch = " o "
+            elif place in comp:
+                ch = " O "
+                
+            row = row + ch
+            place = place + 1
+        print(x, " ", row)
+
+hit = [21,22]
+miss = [20,24,12,13]
+comp = [23] 
+
+shot = get_shot()
+battle_board(hit,miss,comp)
+        
