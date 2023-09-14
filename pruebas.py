cb5d61442f92904023f9ff19e0c351b0e5b95de7
Jorge Pallares Catalan <jorge.pallares.catalan@gmail.com>
2023-09-14 23:09:10 +0200

    redoing battle diagram

--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,23 @@
-#variables for the game
-x = 0
-y = 0
-matrix_xy = [(x, y) for x in range(10) for y in range(10)]
-print(matrix_xy)
+shots_rec = []
+shots_rec.append(1)
+shots_rec.append(5)
+
+ship1_size2 = []
+ship1_size2.append(1)
+ship1_size2.append(2)
+
+i = 0
+cad = ""
+for x in range(99):
+    if x in shots_rec:
+        if x in ship1_size2:
+            cad = cad + " o "
+        else:
+            cad = cad + " O "
+    else:
+        cad = cad + " x "
+    i = i + 1
+    if i > 9:
+        print(cad)
+        i = 0
+        cad = ""
--- pruebas2.py
+++ pruebas2.py
@@ -1,20 +1,23 @@
-ship1_size2 = [1]
-ship2_size2 = [2]
-ship3_size3 = [3]
-ship4_size3 = [4]
-ship5_size4 = [5]
-ship6_size5 = [6]
 shots_rec = []
+shots_rec.append(1)
+shots_rec.append(5)
 
+ship1_size2 = []
+ship1_size2.append(1)
+ship1_size2.append(2)
 
-ship_1 = []
-ship = []
-
-ship.append(10)
-ship.append(11)
-
-ship_1.append(ship)
-ship_1.append(ship)
-
-print(ship)
-print(ship_1)
+i = 0
+cad = ""
+for x in range(99):
+    if x in shots_rec:
+        if x in ship1_size2:
+            cad = cad + " o "
+        else:
+            cad = cad + " O "
+    else:
+        cad = cad + " x "
+    i = i + 1
+    if i > 9:
+        print(cad)
+        i = 0
+        cad = ""
