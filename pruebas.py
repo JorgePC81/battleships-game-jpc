b8fd5c9282ae4a4e9de922a07b23d4f983f262ef
Jorge Pallares Catalan <jorge.pallares.catalan@gmail.com>
2023-09-14 20:45:51 +0200

    fixing matrix for the code, looking at the possibilities with the matrix

--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,5 @@
 #variables for the game
-
-ship1_size2 = [1]
-ship2_size2 = [2]
-ship3_size3 = [3]
-ship4_size3 = [4]
-ship5_size4 = [5]
-ship6_size5 = [6,7]
-
-player_ships = ship1_size2 + ship2_size2 + ship3_size3 + ship4_size3 + ship5_size4 + ship6_size5
-
-print(player_ships)
+x = 0
+y = 0
+matrix_xy = [(x, y) for x in range(10) for y in range(10)]
+print(matrix_xy)
--- run.py
+++ run.py
@@ -8,10 +8,9 @@ ship5_size4 = []
 ship6_size5 = []
 FLOW = "stop"
 shots_rec = []
-x = []
-y = []
-
-
+x = 0
+y = 0
+matrix_xy = [(x, y) for x in range(10) for y in range(10)]
 
 def battle_board():
     """
@@ -52,13 +51,12 @@ def ships_choice():
     while ak == 'y':
         try:
             ship_choice1 = input("ship 1, section 1. Coordenates X, Y (I.E: 3, 5): ")
-            if ship_choice1 >= 0 and ship_choice1 <= 9:
-                ship1_size2.append(ship_choice1)
-                if int(len(ship1_size2)) < 2:
-                    ship_choice2 = input("ship 1, section 2. Coordenates X, Y (I.E: 3, 5): ")
-                    ship1_size2.append(ship_choice2)
-                    if int(len(ship1_size2)) == 2:
-                        print("Ship placed. Please, locate the next ship")
+            ship1_size2.append(ship_choice1)
+            if int(len(ship1_size2)) < 2:
+                ship_choice2 = input("ship 1, section 2. Coordenates X, Y (I.E: 3, 5): ")
+                ship1_size2.append(ship_choice2)
+                if int(len(ship1_size2)) == 2:
+                    print("Ship placed. Please, locate the next ship")
                         
                     
         except ValueError:
@@ -78,17 +76,15 @@ def ships_choice():
         
         try:
             ship_choice5 = input("ship 3, section 1. Coordenates X, Y (I.E: 3, 5): ")
-            if ship_choice5 >= 0 and ship_choice5 <= 9:
-                ship3_size3.append(ship_choice5)
-                if len(ship3_size3) < 3:
-                    ship_choice6 = input("ship 3, section 2. Coordenates X, Y (I.E: 3, 5): ")
-                    ship3_size3.append(ship_choice6)
-                if int(len(ship3_size3)) < 3:
-                    ship_choice7 = input("ship 3, section 3. Coordenates X, Y (I.E: 3, 5): ")
-                    if ship_choice7 >= 0 and ship_choice7 <= 9:
-                        ship3_size3.append(ship_choice7)
-                        if int(len(ship3_size3)) == 3:
-                            print('Ship placed. Please, locate the next ship')  
+            ship3_size3.append(ship_choice5)
+            if len(ship3_size3) < 3:
+                ship_choice6 = input("ship 3, section 2. Coordenates X, Y (I.E: 3, 5): ")
+                ship3_size3.append(ship_choice6)
+            if int(len(ship3_size3)) < 3:
+                ship_choice7 = input("ship 3, section 3. Coordenates X, Y (I.E: 3, 5): ")
+                ship3_size3.append(ship_choice7)
+                if int(len(ship3_size3)) == 3:
+                    print('Ship placed. Please, locate the next ship')  
                                 
         except ValueError:
             print("incorrect entry. Please try again")
@@ -96,15 +92,13 @@ def ships_choice():
         
         try:
             ship_choice8 = input("ship 4, section 1. Coordenates X, Y (I.E: 3, 5): ")
-            if ship_choice8 >= 0 and ship_choice8 <= 9:
-                ship4_size3.append(ship_choice8)
-                if int(len(ship4_size3)) < 3:
-                    ship_choice9 = input("ship 4, section 2. Coordenates X, Y (I.E: 3, 5): ")
-                    ship4_size3.append(ship_choice9)
+            ship4_size3.append(ship_choice8)
+            if int(len(ship4_size3)) < 3:
+                ship_choice9 = input("ship 4, section 2. Coordenates X, Y (I.E: 3, 5): ")
+                ship4_size3.append(ship_choice9)
                 if len(ship4_size3) < 3:
                     ship_choice10 = input("ship 4, section 3. Coordenates X, Y (I.E: 3, 5): ")
-                    if ship_choice10 >= 0 and ship_choice10 <= 9:
-                        ship4_size3.append(ship_choice10)
+                    ship4_size3.append(ship_choice10)
                     if int(len(ship4_size3)) == 3:
                         print('Ship placed. Please, locate the next ship')
                                     
@@ -114,15 +108,13 @@ def ships_choice():
     
         try:
             ship_choice11 = input("ship 5, section 1. Coordenates X, Y (I.E: 3, 5): ")
-            if ship_choice11 >= 0 and ship_choice11 <= 9:
-                ship5_size4.append(ship_choice11)
-                if int(len(ship5_size4)) < 4:
-                    ship_choice12 = input("ship 5, section 2. Coordenates X, Y (I.E: 3, 5): ")
-                    ship5_size4.append(ship_choice12)
+            ship5_size4.append(ship_choice11)
+            if int(len(ship5_size4)) < 4:
+                ship_choice12 = input("ship 5, section 2. Coordenates X, Y (I.E: 3, 5): ")
+                ship5_size4.append(ship_choice12)
                 if len(ship5_size4) < 4:
                     ship_choice13 = input("ship 5, section 3. Coordenates X, Y (I.E: 3, 5): ")
-                    if ship_choice13 >= 0 and ship_choice10 <= 9:
-                        ship5_size4.append(ship_choice13)
+                    ship5_size4.append(ship_choice13)
                 if len(ship5_size4) < 4:
                     ship_choice14 = input("ship 5, section 4. Coordenates X, Y (I.E: 3, 5): ")
                     if ship_choice14 >= 0 and ship_choice14 <= 9:
@@ -136,23 +128,19 @@ def ships_choice():
         
         try:
             ship_choice15 = input("ship 6, section 1. Coordenates X, Y (I.E: 3, 5): ")
-            if ship_choice15 >= 0 and ship_choice15 <= 9:
-                ship6_size5.append(ship_choice15)
-                if int(len(ship6_size5)) < 5:
-                    ship_choice16 = input("ship 6, section 2. Coordenates X, Y (I.E: 3, 5): ")
-                    ship6_size5.append(ship_choice16)
+            ship6_size5.append(ship_choice15)
+            if int(len(ship6_size5)) < 5:
+                ship_choice16 = input("ship 6, section 2. Coordenates X, Y (I.E: 3, 5): ")
+                ship6_size5.append(ship_choice16)
                 if len(ship6_size5) < 5:
                     ship_choice17 = input("ship 6, section 3. Coordenates X, Y (I.E: 3, 5): ")
-                    if ship_choice17 >= 0 and ship_choice17 <= 9:
-                        ship6_size5.append(ship_choice17)
+                    ship6_size5.append(ship_choice17)
                 if len(ship6_size5) < 5:
                     ship_choice18 = input("ship 6, section 4. Coordenates X, Y (I.E: 3, 5): ")
-                    if ship_choice18 >= 0 and ship_choice18 <= 9:
-                        ship6_size5.append(ship_choice18)
+                    ship6_size5.append(ship_choice18)
                 if len(ship6_size5) < 5:
                     ship_choice19 = input("ship 6, section 5. Coordenates X, Y (I.E: 3, 5): ")
-                    if ship_choice19 >= 0 and ship_choice19 <= 9:
-                        ship6_size5.append(ship_choice19)    
+                    ship6_size5.append(ship_choice19)    
                     if int(len(ship6_size5)) == 5:
                        print('ALL YOUR SHIPS ARE ON THE WATER. PREPARE TO SHOT!')
                        print(battle_board)
@@ -167,20 +155,17 @@ def get_shot():
     FLOW = "stop"
     while FLOW == "stop":
         try:
-            shot = input("please enter your attack: "))
-            if shot < 0 or shot > 9:
-                print("incorrect entry, try again")
-            else:
-                shots_rec.append(shot)
-                FLOW = "run"
-                while FLOW == "run":
-                    if shot not in ship1_size2:
-                        sp = " x " #watter
-                    else:
-                        sp = " o "
+            shot = input("please enter your X and Y coordinates: ")
+            shots_rec.append(shot)
+            FLOW = "run"
+            while FLOW == "run":
+                if shot not in ship1_size2:
+                    sp = " x " #watter
+                else:
+                    sp = " o "
                     #elif shot in ship1_size2 and len(ship1_size2) == 1 or len(ship2_size2) ==1 or len(ship3_size3) == 2 or len(ship4_size3) == 2 or len(ship5_size4) == 3 or len(ship6_size5) == 4:
                         #sp = " o " #damage
-                        print("You damaged a ship")
+                    print("You damaged a ship")
                         #if len(ship1_size2) == 2 or len(ship2_size2) == 2 or len(ship3_size3) == 3 or len(ship4_size3) == 3 or len(ship5_size4) == 4 or len(ship6_size5) == 5:
                             #sp = " O " #sank ship
                             #print("Your ship is sank")
